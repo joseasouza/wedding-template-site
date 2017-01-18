610e2ac454927443dc98bab947441dcade15e5b5
Victor <victor_rpg05@hotmail.com>
2017-01-18 00:07:35 -0300

    Finishes implementation of filter functionality and start to implement the dialog when an item is clicked

--- src/gifts-list/controller.js
+++ src/gifts-list/controller.js
@@ -4,14 +4,26 @@
 (function () {
     var app = angular.module('app');
     app.controller("GiftListController", ['$http','$window', '$scope', function($http, $window, $scope) {
+        //TODO needs refatoration
         this.products = [];
         this.showFilterButton = false;
         this.search = "";
         this.buyFromInternet = false;
         this.buyLocal = false;
+        this.brideFav = false;
+        this.groomFav = false;
+        this.chipIn = false;
+        this.householdAppliance = false;
+        this.furnitures = false;
+        this.kitchen = false;
+        this.livingRoom = false;
+        this.room = false;
         this.costRange = [0, 1000];
         this.filterByCategory = filterByCategory;
         this.filterByPrice = filterByPrice;
+        this.showModal = false;
+        this.clickItem = clickItem;
+        this.selectedProduct = {};
         var ctrl = this;
 
         $http.get("/products.json").then(function(data) {
@@ -34,7 +46,7 @@
 
         angular.element($window).bind("scroll", function() {
             var navWrap = $('#search').offset().top;
-            var result = this.pageYOffset >= navWrap - 160;
+            var result = this.pageYOffset >= navWrap - 300;
             if($scope.giftListCtrl.showFilterButton != result) {
                 $scope.giftListCtrl.showFilterButton = result;
                 $scope.$apply();
@@ -42,9 +54,24 @@
         });
 
         function filterByCategory(product) {
-            return product.tags.indexOf("buy-from-internet") >= 0 && ctrl.buyFromInternet
-                || product.tags.indexOf("buy-local") >= 0 && ctrl.buyLocal
-                || !ctrl.buyLocal && !ctrl.buyFromInternet;
+            return isFilteredByCategory(product, Categories.BUY_FROM_INTERNET, ctrl.buyFromInternet)
+                || isFilteredByCategory(product, Categories.BUY_LOCAL, ctrl.buyLocal)
+                || isFilteredByCategory(product, Categories.BRIDE_FAV, ctrl.brideFav)
+                || isFilteredByCategory(product, Categories.GROOM_FAV, ctrl.groomFav)
+                || isFilteredByCategory(product, Categories.CHIP_IN, ctrl.chipIn)
+                || isFilteredByCategory(product, Categories.HOUSEHOLD_APPLIANCE, ctrl.householdAppliance)
+                || isFilteredByCategory(product, Categories.FURNITURES, ctrl.furnitures)
+                || isFilteredByCategory(product, Categories.KITCHEN, ctrl.kitchen)
+                || isFilteredByCategory(product, Categories.LIVING_ROOM, ctrl.livingRoom)
+                || isFilteredByCategory(product, Categories.ROOM, ctrl.room)
+                || !ctrl.buyLocal && !ctrl.buyFromInternet && !ctrl.brideFav
+                    && !ctrl.groomFav && !ctrl.chipIn && !ctrl.householdAppliance
+                    && !ctrl.furnitures && !ctrl.kitchen && !ctrl.livingRoom
+                    && !ctrl.room;
+        }
+
+        function isFilteredByCategory(product, category, model) {
+            return product.tags.indexOf(category) >= 0 && model;
         }
 
         function filterByPrice(product) {
@@ -52,6 +79,11 @@
             var productCost = Number(product.cost);
             return productCost >= Number(priceRange[0])&& productCost < Number(priceRange[1]);
 
+        }
+
+        function clickItem(product) {
+            ctrl.selectedProduct = product;
+            ctrl.showModal = true;
         }
 
         $("#filterGiftsBtn").sideNav({
@@ -62,13 +94,15 @@
         });
 
         slider.noUiSlider.on("update", function() {
-            var valor = "R$" + slider.noUiSlider.get()[0] + ",00 até R$" + slider.noUiSlider.get()[1] + ",00";
+            var valor = "De R$" + slider.noUiSlider.get()[0] + ",00 até R$" + slider.noUiSlider.get()[1] + ",00";
             $("#valor-range").html(valor);
         });
         slider.noUiSlider.on("change", function() {
             ctrl.costRange = slider.noUiSlider.get();
             $scope.$apply();
         });
+
+        $('.modal').modal();
 
     }]);
 
---
+++ src/js/Categories.js
@@ -0,0 +1,15 @@
+/**
+ * Created by victor on 17/01/17.
+ */
+var Categories = {
+    HOUSEHOLD_APPLIANCE: "household-appliance",
+    KITCHEN: "kitchen",
+    FURNITURES: "furnitures",
+    BRIDE_FAV: "bride-favorites",
+    GROOM_FAV: "groom-favorites",
+    BUY_FROM_INTERNET: "buy-from-internet",
+    BUY_LOCAL: "buy-local",
+    LIVING_ROOM: "living-rooom",
+    ROOM: "room",
+    CHIP_IN: "CHIP_IN"
+};

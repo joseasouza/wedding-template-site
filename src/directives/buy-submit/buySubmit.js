c82bc2f377bac581ebc7ab1f3ce2b193d47de4f7
Victor <victor_rpg05@hotmail.com>
2017-01-23 00:12:49 -0300

    Finishes implementation of PagSeguro payment method. Update gifts list layout to turn it responsive

---
+++ src/directives/buy-submit/buySubmit.js
@@ -0,0 +1,32 @@
+(function () {
+    var app = angular.module("app");
+    app.directive("buySubmit", function () {
+        return {
+            restrict: 'E',
+            templateUrl: "/src/directives/buy-submit/",
+            scope: {
+                price: '=price',
+                productName: '=productName',
+                amount: '=amount',
+                form: '@form'
+            }
+        };
+    });
+
+    var SubmitController = function($scope, $element) {
+        $element.bind('click', function() {
+            $("#" + $scope.form).submit();
+        });
+    };
+
+    app.controller("SubmitController", ['$scope', '$element', SubmitController]);
+    app.directive("submit", function () {
+        return {
+            restrict: 'A',
+            scope: {
+                form: '@form'
+            },
+            controller: 'SubmitController'
+        }
+    });
+})();
